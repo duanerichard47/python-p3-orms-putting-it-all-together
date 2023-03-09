4d2a77b4e89dbe5a28461a6ed33b876b9d46c568
Duane <duanerichard47@hotmail.com>
2023-03-09 00:11:07 -0500

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -5,11 +5,13 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    def __init__(self, name, breed, id = None):
+    def __init__(self, name, breed, id = None):    #instance with created without name and bread
         self.id = id
         self.name = name
         self.breed = breed
-        
+
+    all_dogs = []  
+                                # table if first thing(second thing overall) after instance that is created. Can't add data before table created
     @classmethod                #class method not instance method. Want class creating table so all methods would be able to too
     def create_table(cls):          #lab specifically asks to create instance dogs
         sql = """
@@ -27,31 +29,41 @@ class Dog:
         """
         CURSOR.execute(sql)
 
+  
     def save(self):
         sql = """
             INSERT INTO dogs (name, breed)
             VALUES (?, ?)
         """
-        CURSOR.execute(sql)
+        
+        CURSOR.execute(sql,(self.name,self.breed))  #allows to read string as SQL commands and passes values into sql statement
 
-    # def create(self,name, breed):
-    #     self = Dog(name, breed)
-    #     self.save()
-    #     return self
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM dogs").fetchone()[0]  # after instance saved( we can't create an id database does when saved) grabbing the id created by database and adding it to the python created instance
 
-    #     CURSOR.execute(sql)
-
-    # def new_from_db(cls, row):
-    #     song = cls(row[1], row[2])
-    #     song.id = row[0]
-    #     return song
     
-    # def get_all(self):
-    #     sql = """
-    #         SELECT *
-    #         FROM self
-    #     """
-   
+    @classmethod
+    def create(cls ,name, breed):           #3rd and 4th step. 1st row created. values created
+        new_dog = Dog(name, breed)
+        new_dog.save()                      #automatically called and values are added to table
+        return new_dog
+
+        
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+        return song
+    
+    @classmethod
+    def get_all(cls):                # retrieving all data from table. All multiple rows
+        sql = """
+            SELECT *
+            FROM dogs
+        """
+        all_dogs = CURSOR.execute(sql).fetchall()
+
+        cls.all_dogs = [cls.new_from_db(row) for row in all_dogs]  #retrieves data row by row into list of dictionaries
+        return cls.all_dogs
 
 
     # def find_by_name(self):
